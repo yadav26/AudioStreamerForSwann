a1689777894fa3108635373aede64479bf1715d0
Anshul Yadav <yadav26@gmail.com>
2024-05-08 14:32:27 +1000

    as thread, termination StartConnection has crash

--- AudioStreamerForSwann/main.cpp
+++ AudioStreamerForSwann/main.cpp
@@ -9,14 +9,24 @@ extern int StartConnection(std::string);
 
 int main()
 {
-    int port = 6587;
+    int port = 8765;
     std::string wsserverip = "3.25.140.218:8765";
 
-    StartConnection(wsserverip);
-    WaveRecorder wrc;
-    wrc.start();
+    std::thread th([&]() {
+        StartConnection(wsserverip);
+        }
+    );
 
-    wrc.stop();
+    std::thread thAudio([&]() {
+            WaveRecorder wrc;
+            wrc.start();
+        }
+    );
+    thAudio.join();
+
+
+    
+    th.join();
     return 0;
 }
 
