a33779df3d348666483176025f11547e2c5b86bf
Anshul Yadav <yadav26@gmail.com>
2024-05-08 14:54:58 +1000

    closing comments

--- AudioStreamerForSwann/main.cpp
+++ AudioStreamerForSwann/main.cpp
@@ -12,23 +12,26 @@ std::atomic_bool States(false);
 int main()
 {
     int port = 8765;
-    std::string wsserverip = "3.25.140.218:8765";
+    std::string wsserverip = "wss://3.25.140.218:8765";
 
     std::thread th([&]() {
+        //here it should establish wss client connection
         StartConnection(wsserverip);
         }
     );
 
+
     std::thread thAudio([&]() {
+            //below we will get the audio buffer recorded into file if 
+            //system is disconnected state
             WaveRecorder wrc;
             wrc.start();
         }
     );
     thAudio.join();
-
-
-    
     th.join();
+
+    //Fix: termination crash - anshul 
     return 0;
 }
 
